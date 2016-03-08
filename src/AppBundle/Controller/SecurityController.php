c3c896172373f718bf505b87cfa49c6359f3312f
Denis Beliakov <denis.beliakov14@gmail.com>
2016-03-08 14:55:58 +0200

    added: registration organization

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Organization;
 use AppBundle\Entity\Person;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -24,11 +25,11 @@ class SecurityController extends Controller
     }
 
     /**
-     * @Route("/register", name="registration")
+     * @Route("/register-person", name="registration_person")
      * @param Request $request
      * @return Response
      */
-    public function registerAction(Request $request)
+    public function registerPersonAction(Request $request)
     {
         $user = new Person();
         $form = $this->createForm(RegisterPersonType::class, $user);
@@ -48,7 +49,41 @@ class SecurityController extends Controller
                 $em->flush();
 
                 return $this->redirectToRoute(
-                    "registration"
+                    "registration_person"
+                );
+            }
+        }
+        return $this->render('@App/security/register-person.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/register-organization", name="registration_organization")
+     * @param Request $request
+     * @return Response
+     */
+    public function registerOrganizationAction(Request $request)
+    {
+        $user = new Organization();
+        $form = $this->createForm(RegisterPersonType::class, $user);
+
+        if ($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+
+            if ($form->isValid()) {
+//                $password = $this->get('security.password_encoder')
+//                    ->encodePassword($user, $user->getPlainPassword());
+                $user->setPassword($user->getPlainPassword());
+
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $uploadableManager = $this->get('stof_doctrine_extensions.uploadable.manager');
+                $uploadableManager->markEntityToUpload($user, $user->getAvatarFileName());
+                $em->flush();
+
+                return $this->redirectToRoute(
+                    "registration_organization"
                 );
             }
         }
---
+++ src/AppBundle/Form/RegisterOrganizationType.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class RegisterPersonType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class)
+            ->add('plainPassword', RepeatedType::class, array(
+                    'type' => PasswordType::class,
+                    'first_options'  => array('label' => 'Password'),
+                    'second_options' => array('label' => 'Repeat Password'),
+                )
+            )
+            ->add('email', EmailType::class, array(
+                'attr' => array('class' => 'form-control')
+            ))
+            ->add('avatarFileName', FileType::class, array('required' => false, 'data_class' => null, 'mapped' => true))
+            ->add('role', TextType::class)
+            ->add('bankDetails', TextareaType::class)
+            ->add('address', TextType::class)
+            ->add('phone', TextType::class)
+            ->add('categories', 'entity', array(
+                'class' => 'AppBundle\Entity\Category',
+                'choice_label' => 'title',
+                'multiple' => 'true',
+            ))
+            ->add('showOtherCategories', CheckboxType::class, array(
+                //TODO: translations
+                'label'    => 'Отображать другие категории?',
+                'required' => false,
+            ))
+            ->add('followCategories', 'entity', array(
+                'class' => 'AppBundle\Entity\Category',
+                'choice_label' => 'title',
+                'multiple' => 'true',
+            ))
+            ->add('organizationName', TextType::class)
+            ->add('organizationDocuments', TextareaType::class)
+            ->add('activityProfile', TextType::class)
+            ->add('website', TextType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Organization'
+        ));
+    }
+}
