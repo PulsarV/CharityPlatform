ea7416478ec47d1b313b2aa7522db9e843a1a883
Volodymyr Kravchuk <volodymyr.a.kravchuk@gmail.com>
2016-03-13 20:28:01 +0200

    fixed tinymce download and connect

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 var gulp = require('gulp'),
- less = require('gulp-less'),
- clean = require('gulp-clean');
- concatJs = require('gulp-concat'),
- minifyJs = require('gulp-uglify');
+    less = require('gulp-less'),
+    clean = require('gulp-clean'),
+    concatJs = require('gulp-concat'),
+    minifyJs = require('gulp-uglify');
 gulp.task('less', function() {
  return gulp.src(['web-src/less/*.less', 'web-src/less/*.css'])
  .pipe(less({compress: true}))
@@ -10,19 +10,19 @@ gulp.task('less', function() {
 });
 gulp.task('images', function () {
  return gulp.src([
- 'web-src/images/*',
+  'web-src/images/*'
  ])
  .pipe(gulp.dest('web/images/'))
 });
 gulp.task('images-ico', function () {
  return gulp.src([
- 'web-src/images/ico/*',
+  'web-src/images/ico/*'
  ])
  .pipe(gulp.dest('web/images/ico/'))
 });
 gulp.task('images-test', function () {
  return gulp.src([
- 'web-src/images/test/*',
+  'web-src/images/test/*'
  ])
  .pipe(gulp.dest('web/images/test/'))
 });
@@ -32,31 +32,37 @@ gulp.task('fonts', function () {
 });
 gulp.task('lib-js', function() {
  return gulp.src([
- 'bower_components/jquery/dist/jquery.js',
- 'bower_components/bootstrap/dist/js/bootstrap.js'
+  'bower_components/jquery/dist/jquery.js',
+  'bower_components/bootstrap/dist/js/bootstrap.js'
  ])
  .pipe(concatJs('app.js'))
  .pipe(minifyJs())
  .pipe(gulp.dest('web/js/'));
 });
-gulp.task('tinymce-js', function() {
+gulp.task('tinymce', function() {
  return gulp.src([
-      'bower_components/tinymce/tinymce.js'
-     ])
-     .pipe(concatJs('tinymce.js'))
-     .pipe(minifyJs())
-     .pipe(gulp.dest('web/js/'));
+  'bower_components/tinymce/**/*.*'
+ ])
+ .pipe(gulp.dest('web/tinymce/'));
 });
 gulp.task('clean', function () {
- return gulp.src(['web/css/*', 'web/js/*', 'web/images/*', 'web/fonts/*'])
+ return gulp.src([
+  'web/css/*', 'web/js/*', 'web/images/*', 'web/fonts/*', 'web/tinymce/*'
+ ])
  .pipe(clean());
 });
 gulp.task('default', ['clean'], function () {
- var tasks = ['images', 'images-ico', 'images-test', 'fonts', 'less', 'lib-js', 'tinymce-js'];
+ var tasks = [
+  'images', 'images-ico', 'images-test', 'fonts', 'less', 'lib-js', 'tinymce'
+ ];
  tasks.forEach(function (val) {
- gulp.start(val);
+  gulp.start(val);
  });
 });
 gulp.task('watch', function () {
- var less = gulp.watch(['web-src/less/*.less', 'web-src/less/*.css'], ['less'])
+ var less = gulp.watch([
+  'web-src/less/*.less', 'web-src/less/*.css'
+ ], [
+  'less'
+ ])
 });
