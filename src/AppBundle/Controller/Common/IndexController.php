8f86b694406a2f4b7510f3171399899c0e595412
Denis Beliakov <denis.beliakov14@gmail.com>
2016-03-25 10:44:15 +0200

    added: mailsender

--- src/AppBundle/Controller/Common/IndexController.php
+++ src/AppBundle/Controller/Common/IndexController.php
@@ -14,4 +14,25 @@ class IndexController extends Controller
     {
         return $this->redirectToRoute('charity_index');
     }
+
+    /**
+     * @Route("/test-mail", name="test_mail")
+     */
+    public function sendTestMailAction()
+    {
+        $mail = $this->get('mail');
+        $mailSender = 'gh.charity.supp@gmail.com';
+        $targetMail = 'workbel14@gmail.com';
+        $mailTitle = 'Yea! Test mail!';
+        $mail->send(
+            $mailSender,
+            $targetMail,
+            $mailTitle,
+            'AppBundle:Emails:testmail.html.twig',
+            array(
+                'mailsender' => $mailSender
+            )
+        );
+        return $this->redirectToRoute('charity_index');
+    }
 }
